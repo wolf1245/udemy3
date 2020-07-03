d1b6d29068aff0ee52e5ddddcdc36d97e1eab939
Wolf <wolf22396@gmail.com>
2020-07-03 17:31:14 +0300

    Ñäåëàëè 2 âåðñèè ñëàéäåðà, à òàêæå òî÷êè ïåðåêëþ÷åíèÿ

--- js/main.js
+++ js/main.js
@@ -336,12 +336,14 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     // ф-я запроса fecth промиса
-    async function getResource(url) {
+    async function getResource(url) 
+    {
         // переменная fecth промиса
         let res = await fetch(url);
         
         // проверяем ответ fecth промиса
-        if (!res.ok) {
+        if (!res.ok) 
+        {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
         
@@ -350,7 +352,8 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     //  обработки форм
-    function postData(form) {
+    function postData(form) 
+    {
         // при собитии отправить
         form.addEventListener('submit', (event) => {
             // сообщаем что событие не обработанно
@@ -401,7 +404,8 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     // ф-я вывода ответа на запрос пользователю
-    function showThanksModal(message) {
+    function showThanksModal(message) 
+    {
         // получаем модельное окно куда будем выводить
         const prevModalDialog = document.querySelector('.modal__dialog');
 
@@ -434,5 +438,316 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000);
     }
-    /// AJAX end
+    // AJAX end
+    /*
+    // Slider v1 start
+    // индекс слайда для нас с единицы, для жс с нуля
+    let slideIndex = 1;
+    // получаем псевдомассив с слайдами
+    const slides = document.querySelectorAll('.offer__slide'),
+        // предыдущий слайд
+        prev = document.querySelector('.offer__slider-prev'),
+        // последующий слайд
+        next = document.querySelector('.offer__slider-next'),
+        // получаем элемент показывающий общее к-во слайдов
+        total = document.querySelector('#total'),
+        // получаем элемент вывода текущего слайда 
+        current = document.querySelector('#current');
+
+    // запуск ф-и для скрытия всех и отображения 1 слайда
+    showSlides(slideIndex);
+
+    // проверяем ко-во слайдов в пседомассиве 
+    if (slides.length < 10) 
+    {
+        //если меньше  10 добавляем ноль перед числом
+        total.textContent = `0${slides.length}`;
+    } 
+    else 
+    {
+        // если больше выводим так
+        total.textContent = slides.length;
+    }
+
+    // ф-я показа нужного слайда
+    function showSlides(n) 
+    {
+        // возращаем на 1 слайд
+        if (n > slides.length) 
+        {
+            slideIndex = 1;
+        }
+        // возращаем на последний слайд
+        if (n < 1) 
+        {
+            slideIndex = slides.length;
+        }
+
+        // выключаем отображения свойства цсс у слайдов
+        slides.forEach((item) => item.style.display = 'none');
+
+        // включаем показ того слайда на котором находимся
+        slides[slideIndex - 1].style.display = 'block'; // Как ваша самостоятельная работа - переписать на использование классов show/hide
+        
+        // текущий номер слайда
+        if (slides.length < 10) 
+        {
+            // если меньше 10, добавляем ноль перед числом
+            current.textContent =  `0${slideIndex}`;
+        } 
+        else 
+        {
+            current.textContent =  slideIndex;
+        }
+    }
+
+    // ф-я переключения слайда 
+    function plusSlides (n) 
+    {
+        showSlides(slideIndex += n);
+    }
+
+    // предыдущий слайд
+    prev.addEventListener('click', function(){
+        plusSlides(-1);
+    });
+
+    // следующий слайд
+    next.addEventListener('click', function(){
+        plusSlides(1);
+    });
+    // Slider v1 end
+    */
+    // Slider v2 start
+    // отступ
+    let offset = 0;
+    // индекс слайда для нас с единицы, для жс с нуля
+    let slideIndex = 1;
+
+    // получаем псевдомассив с слайдами
+    const slides = document.querySelectorAll('.offer__slide'),
+        // достаем слайдер
+        slider = document.querySelector('.offer__slider'),
+        // предыдущий слайд
+        prev = document.querySelector('.offer__slider-prev'),
+        // последующий слайд
+        next = document.querySelector('.offer__slider-next'),
+        // получаем элемент показывающий общее к-во слайдов
+        total = document.querySelector('#total'),
+        // получаем элемент вывода текущего слайда 
+        current = document.querySelector('#current'),
+        // получаем обертку слайдера
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        // получаем приминеную ширину к обертке
+        width = window.getComputedStyle(slidesWrapper).width,
+        // контейнер поле наших слайдеров
+        slidesField = document.querySelector('.offer__slider-inner');
+
+    // если слайд меньше 10
+    if (slides.length < 10) 
+    {
+        // добавляем ноль перед цифрой
+        total.textContent = `0${slides.length}`;
+        // добавляем перед цифрой индексу
+        current.textContent =  `0${slideIndex}`;
+    } 
+    else 
+    {
+        total.textContent = slides.length;
+        current.textContent =  slideIndex;
+    }
+    
+    // устаннавливаем ему ширину,на все слайды
+    slidesField.style.width = 100 * slides.length + '%';
+    // ставим в ряд слайды
+    slidesField.style.display = 'flex';
+    // делаем плавный преход
+    slidesField.style.transition = '0.5s all';
+
+    // скрываем все выходящие за границу сладйы с полем
+    slidesWrapper.style.overflow = 'hidden';
+
+    // устанавливаем всем слайдам одинаковую ширину
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    // добавляем слайдеру цсс свойство
+    slider.style.position = 'relative';
+
+    // создаем список ол
+    const indicators = document.createElement('ol'),
+        // создаем массив
+        dots = [];
+    // добавляем класс
+    indicators.classList.add('carousel-indicators');
+    // добавляем стили цсс. если добавим в цсс файл то не будет доступа
+    indicators.style.cssText = `
+        position: absolute;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        z-index: 15;
+        display: flex;
+        justify-content: center;
+        margin-right: 15%;
+        margin-left: 15%;
+        list-style: none;
+    `;
+    // вставляем в слайдер в конец
+    slider.append(indicators);
+
+    // считаем количество слайдеров и создаем такое же количество ли
+    for (let i = 0; i < slides.length; i++) 
+    {
+        // создаем тег ли
+        const dot = document.createElement('li');
+        // добавляем атрибут, начиная с 1
+        dot.setAttribute('data-slide-to', i + 1);
+        // добавляем стили
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        // 
+        if (i == 0) 
+        {
+            // делаем не прозразным
+            dot.style.opacity = 1;
+        }
+        // добавляем в ол список ли
+        indicators.append(dot);
+        // добавляем ли в массив
+        dots.push(dot);
+    }
+
+    // ф-я анимации переключения точек слайда
+    function arrDots(arr)
+    {
+        // делаем каждой лишке прозрачность на половину
+        arr.forEach(dot => dot.style.opacity = ".5");
+        // делаем видимым ту ли которая равна слайдеру
+        arr[slideIndex-1].style.opacity = 1;
+    }
+
+    //ф-я добавления 0 если индекс меньше 10
+    function plusZero(count)
+    {
+        if (count.length < 10) 
+        {
+            // если меньше 10, добавляем ноль перед числом
+            return current.textContent =  `0${slideIndex}`;
+        } 
+        else 
+        {
+            return current.textContent =  slideIndex;
+        }
+    }
+    //обработка клика
+    next.addEventListener('click', () => {
+        /**
+         * если наш отступ будет равен умноженоое на количество
+         * слайдев минус 1, то устанавлюем offset
+         * чтоб вернуться в начало
+         * обязательно унарный плюс превращаем строку '500px' в число +width.slice(0, width.length - 2)
+         */
+        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) 
+        {
+            offset = 0;
+        } 
+        else 
+        {
+            // если не последний слайд добавляем смещение
+            offset += +width.slice(0, width.length - 2); 
+        }
+
+        // передаем на сколько нужно здивунуть слайд
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        // если дошли до конца перемещаемся на 1 слайд
+        if (slideIndex == slides.length) 
+        {
+            slideIndex = 1;
+        }
+        // если нет
+        else 
+        {
+            slideIndex++;
+        }
+
+        // запуск ф-и
+        plusZero(slides);
+
+        // запуск ф-и
+        arrDots(dots);
+    });
+
+    // обработка клика
+    prev.addEventListener('click', () => {
+        // если мы на ходимся на первом слайде
+        if (offset == 0) 
+        {
+            // просчитываем то количество на которое сдвинуть
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } 
+        else 
+        {
+            // просчитываем то количество на которое сдвинуть
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        // передаем на сколько нужно здивунуть слайд
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        // если мы на 1 слайде, перемещаемся на последний
+        if (slideIndex == 1) 
+        {
+            slideIndex = slides.length;
+        }
+        // если нет
+        else 
+        {
+            slideIndex--;
+        }
+
+        // запуск ф-и
+        plusZero(slides);
+
+        // запуск ф-и
+        arrDots(dots);
+    });
+
+    // проходимся по массиву
+    dots.forEach(dot => {
+        // при клике на ли 
+        dot.addEventListener('click', (e) => {
+            //при клике на  ли атрибут равен
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            // переназначаем индекс на индекс ли
+            slideIndex = slideTo;
+            // просчитываем на сколько нужно сдвинуть слайдер
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            // сколько нужно сдвинуть
+            slidesField.style.transform = `translateX(-${offset}px)`;
+
+            // запуск ф-и
+            plusZero(slides);
+
+            // запуск ф-и
+            arrDots(dots);
+        });
+    });
+    // Slider v2 end
 });
